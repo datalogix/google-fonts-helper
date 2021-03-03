4262d1c5cc1f177c510235bc9019dca26d5287f5
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-03-03 08:05:14 -0300

    fix: tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   preset: 'ts-jest',
   collectCoverage: true,
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  transform: {
+    '\\.(js|ts)$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          '@babel/preset-typescript'
+        ],
+        plugins: ['@babel/plugin-transform-runtime']
+      }
+    ]
+  }
 }
